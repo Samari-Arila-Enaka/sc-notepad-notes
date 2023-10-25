7d1ed02e0c18569c0c6e84379c78176b30961533
Samari Arila Enaka <samariarilaenaka@gmail.com>
2023-10-25 16:08:50 +0100

    README.md now generated with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,34 @@
 const { src, dest, series } = require('gulp');
 const htmlMinify = require('html-minifier');
+const mustache = require('gulp-mustache');
 
 const options = {
     collapseWhitespace: true,
     removeComments: true,
+    removeAttributeQuotes: true,
 };
+
+const notes = [];
 
 function html() {
     return src('src/*.html')
         .on('data', function(file) {
             const bufferFile = Buffer.from(htmlMinify.minify(file.contents.toString(), options));
+            notes.push({
+                text: `\`\`\`html\n${bufferFile.toString()}\n\`\`\``,
+                name: `\`${file.stem}\``,
+            });
             return file.contents = bufferFile;
         })
         .pipe(dest('dist'));
 }
 
-exports.build = series(html);
+function templateBuild() {
+    return src('template/README.md')
+        .pipe(mustache({
+            "notes": notes,
+        }))
+        .pipe(dest('.'));
+}
+
+exports.build = series(html, templateBuild);
---
+++ template/README.md
@@ -0,0 +1,18 @@
+# sc-notepad-notes
+Notes to copy and paste into in-game notepad
+
+This is just a test for now. you should be able to click on the copy button and then paste into the in-game notepad. Let me know what you think, and if it's worth pursuing.
+
+
+## How to use
+1. Click on the copy button next to the note you want to copy
+2. Open the in-game notepad
+3. Click on the 'New Note' button and give your note a name
+4. click inside the empty note and press Ctrl+V to paste the note
+
+## Notes
+{{#notes}}
+{{{name}}}
+
+{{{text}}}
+{{/notes}}
